0f0de6143b6d4ba0a2f24443e1cb815c78d8cfde
Hillll <lewis_hill240@outlook.com>
2022-10-05 14:47:18 +0100

    Comments

--- spd.sh
+++ spd.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#SBATCH -J spd-solar
-#SBATCH -p sciama3.q
-#SBATCH --nodes=1
-#SBATCH --cpus-per-task=20
-#SBATCH --time=6:00:00
-#SBATCH --array=[0-92]%10
-#SBATCH --mem=0
+#SBATCH -J spd-solar  # name you want to appear on the scima queue
+#SBATCH -p sciama3.q  # partition you want job sent to
+#SBATCH --nodes=1     # how many nodes you want to use. For the MCMC code only 1 node will work
+#SBATCH --cpus-per-task=20  # CPUs to use per node. Put the maximum number depending on what partition you use
+#SBATCH --time=6:00:00      # how long you want the job to run for
+#SBATCH --array=[0-92]%10   # This relates to the input number of the script. %10 means that a max of 10 individual jobs will run at once
+#SBATCH --mem=0             # get max memory from node.
 #SBATCH --mail-type=begin        # send email when job begins
 #SBATCH --mail-type=end          # send email when job ends
-#SBATCH --mail-user=lewis.hill@port.ac.uk
+#SBATCH --mail-user=             # add your email if you want to be notified about jobs running
 
 cd /mnt/lustre/lhill/SPD/
 
